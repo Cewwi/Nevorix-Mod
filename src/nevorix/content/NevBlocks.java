fd38a20b51e1471a742a47a8cd9c7651d9fdef50
exports.cewwi <148647497+Cewwi@users.noreply.github.com>
2024-10-25 17:16:15 -0400

    Update NevBlocks.java

--- src/nevorix/content/NevBlocks.java
+++ src/nevorix/content/NevBlocks.java
@@ -1,3 +1,69 @@
+package nevorix.content;
+
+/* import arc.Core;
+import arc.func.Cons;
+import arc.graphics.Blending;
+import arc.graphics.Color;
+import arc.graphics.g2d.Draw;
+import arc.graphics.g2d.Fill;
+import arc.graphics.g2d.Lines;
+import arc.math.Angles;
+import arc.math.Interp;
+import arc.math.Mathf;
+import arc.math.Rand;
+import arc.math.geom.Vec2;
+import arc.util.Eachable;
+import arc.util.Time;
+import arc.util.Tmp;
+import arc.util.io.Reads;
+import arc.util.io.Writes;
+import mindustry.Vars; */
+import mindustry.content.*;
+import mindustry.entities.*;
+import mindustry.entities.bullet.BasicBulletType;
+import mindustry.entities.bullet.BulletType;
+import mindustry.entities.bullet.PointLaserBulletType;
+import mindustry.entities.bullet.ShrapnelBulletType;
+import mindustry.entities.part.HaloPart;
+import mindustry.entities.part.RegionPart;
+import mindustry.entities.pattern.*;
+import mindustry.entities.units.BuildPlan;
+import mindustry.game.Team;
+import mindustry.gen.*;
+import mindustry.graphics.CacheLayer;
+import mindustry.graphics.Drawf;
+import mindustry.graphics.Layer;
+import mindustry.graphics.Pal;
+import mindustry.type.Category;
+import mindustry.type.ItemStack;
+import mindustry.type.LiquidStack;
+import mindustry.world.Block;
+import mindustry.world.blocks.defense.*;
+import mindustry.world.blocks.defense.turrets.*;
+import mindustry.world.blocks.environment.*;
+import mindustry.world.blocks.liquid.LiquidRouter;
+import mindustry.world.blocks.logic.MessageBlock;
+import mindustry.world.blocks.power.Battery;
+import mindustry.world.blocks.power.ConsumeGenerator;
+import mindustry.world.blocks.power.PowerNode;
+import mindustry.world.blocks.production.AttributeCrafter;
+import mindustry.world.blocks.production.GenericCrafter;
+import mindustry.world.blocks.production.SolidPump;
+import mindustry.world.blocks.sandbox.ItemSource;
+import mindustry.world.blocks.sandbox.LiquidSource;
+import mindustry.world.blocks.sandbox.PowerVoid;
+import mindustry.world.blocks.storage.StorageBlock;
+import mindustry.world.consumers.ConsumeCoolant;
+import mindustry.world.consumers.ConsumeLiquid;
+import mindustry.world.draw.*;
+import mindustry.world.meta.*;
+
+import static arc.graphics.g2d.Draw.color;
+import static arc.graphics.g2d.Lines.lineAngle;
+import static arc.math.Angles.randLenVectors;
+import static mindustry.Vars.*;
+import static mindustry.type.ItemStack.with;
+
 public class NevBlocks{
 
 	public static Block
